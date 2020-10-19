f8bd7ffab8ef844dd023432ae2616fd4b46c610f
Fidan Fayzullin <1fightr1@rambler.ru>
2020-10-19 18:50:50 +0500

    Registration Page created with Vaadin standart components

---
+++ src/main/java/jm/stockx/controller/registration/RegistrationForm.java
@@ -0,0 +1,65 @@
+package jm.stockx.controller.registration;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class RegistrationForm extends VerticalLayout {
+    private static final String googleOAuth2 = "/oauth2/authorization/google";
+    private static final String facebookOAuth2 = "/oauth2/authorization/facebook";
+    private static final String appleOAuth2 = "/oauth2/authorization/apple";
+    private static final String twitterOAuth2 = "/oauth2/authorization/twitter";
+
+    Anchor googleLoginButton = new Anchor(googleOAuth2, "Login with Google");
+    Anchor facebookLoginButton = new Anchor(facebookOAuth2, "Login with Facebook");
+    Anchor appleLoginButton = new Anchor(appleOAuth2, "Login with Apple");
+    Anchor twitterLoginButton = new Anchor(twitterOAuth2, "Login with Twitter");
+    Text text = new Text("- or -");
+
+    TextField firstName = new TextField();
+    TextField lastName = new TextField();
+    EmailField email = new EmailField();
+    PasswordField password = new PasswordField();
+
+    Checkbox checkbox = new Checkbox();
+    Text text1 = new Text("By signing up, you agree to the");
+    Anchor terms = new Anchor("https://stockx.com/terms", "Terms of Service");
+    Text text2 = new Text("and");
+    Anchor privacyPolicy = new Anchor("https://stockx.com/privacy", "Privacy Policy");
+    HorizontalLayout checkboxLayout1 = new HorizontalLayout(checkbox, text1);
+    HorizontalLayout checkboxLayout2 = new HorizontalLayout(terms, text2, privacyPolicy);
+
+    Button signUp = new Button("Sign Up");
+
+    public RegistrationForm() {
+        firstName.setPlaceholder("First Name");
+        lastName.setPlaceholder("Last Name");
+        email.setPlaceholder("Email Address");
+        password.setPlaceholder("Password");
+        signUp.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        signUp.addClickShortcut(Key.ENTER); // defines keyboard shortcut: enter to sign up
+        addClassName("registration-form");
+        add(googleLoginButton,
+                facebookLoginButton,
+                appleLoginButton,
+                twitterLoginButton,
+                text,
+                firstName,
+                lastName,
+                email,
+                password,
+                checkboxLayout1,
+                checkboxLayout2,
+                signUp
+        );
+    }
+}
+
---
+++ src/main/java/jm/stockx/controller/registration/RegistrationView.java
@@ -0,0 +1,29 @@
+package jm.stockx.controller.registration;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("registration")
+@CssImport("./styles/shared-styles.css")
+public class RegistrationView extends VerticalLayout {
+
+    Anchor toMainPage = new Anchor("https://stockx.com", "Back To Main Page");
+    HorizontalLayout backToMainLayout = new HorizontalLayout(toMainPage);
+
+    private RegistrationForm registrationForm;
+
+    public RegistrationView() {
+        addClassName("list-view");
+        setSizeFull();
+
+        registrationForm = new RegistrationForm();
+        registrationForm.setDefaultHorizontalComponentAlignment(
+                FlexComponent.Alignment.CENTER);
+
+        add(backToMainLayout, registrationForm);
+    }
+}
