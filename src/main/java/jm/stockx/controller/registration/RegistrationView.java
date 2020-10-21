36f8c41914d3fb547d5e5809e98ed47bcffd8720
Fidan Fayzullin <1fightr1@rambler.ru>
2020-10-21 18:29:21 +0500

    mapping to registration

--- src/main/java/jm/stockx/controller/registration/RegistrationView.java
+++ src/main/java/jm/stockx/controller/registration/RegistrationView.java
@@ -6,7 +6,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("")
+@Route("registration")
 @CssImport("./styles/shared-styles.css")
 public class RegistrationView extends VerticalLayout {
 
