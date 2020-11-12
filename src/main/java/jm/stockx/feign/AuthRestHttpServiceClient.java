a10637939b679b6c9fa3200e2547f2092402f896
admin <admin>
2020-11-12 03:43:09 +0300

    With Spring Cloud OpenFeign in AuthRestHttpService

---
+++ src/main/java/jm/stockx/feign/AuthRestHttpServiceClient.java
@@ -0,0 +1,15 @@
+package jm.stockx.feign;
+
+import jm.stockx.dto.UserLoginDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(url = "http://localhost:8080")
+public interface AuthRestHttpServiceClient {
+
+    @PostMapping(value = "/auth/login")
+    String getToken(@RequestHeader("Accept") String accept,
+                    @RequestHeader("Content-type") String contentType,
+                    UserLoginDto userLoginDto);
+}
--- src/main/java/jm/stockx/http/service/AuthRestHttpService.java
+++ src/main/java/jm/stockx/http/service/AuthRestHttpService.java
@@ -1,48 +1,28 @@
 package jm.stockx.http.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jm.stockx.dto.UserLoginDto;
-import jm.stockx.dto.UserTokenDto;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.beans.factory.annotation.Value;
+import jm.stockx.feign.AuthRestHttpServiceClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthRestHttpService {
-    @Value("${basic.url}")
-    private String basicUrl;
 
-    private CloseableHttpClient httpClient;
-    private ObjectMapper mapper;
-    private final String postfixUrl = "/auth/login";
+    private final AuthRestHttpServiceClient authRestHttpServiceClient;
 
-    public AuthRestHttpService() {
-        this.httpClient = HttpClients.createDefault();
-        this.mapper = new ObjectMapper();
+    @Autowired
+    public AuthRestHttpService(AuthRestHttpServiceClient authRestHttpServiceClient) {
+        this.authRestHttpServiceClient = authRestHttpServiceClient;
     }
 
     public String getToken(String username, String password) {
         try {
-            String jsonEntity = mapper.writeValueAsString(new UserLoginDto(username, password, true));
-
-            HttpPost httpPost = new HttpPost(basicUrl + postfixUrl);
-            httpPost.setEntity(new StringEntity(jsonEntity));
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-            HttpResponse response = httpClient.execute(httpPost);
-
-            UserTokenDto userTokenDto = mapper.readValue(response.getEntity().getContent(), UserTokenDto.class);
-
-            return userTokenDto.getToken();
+            return authRestHttpServiceClient.getToken("application/json",
+                    "application/json",
+                    new UserLoginDto(username, password, true));
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
     }
-
-
 }
