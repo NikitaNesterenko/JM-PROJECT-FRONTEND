b6b73b78a79072c08830928c6f571a049e33a976
Stanislav <CruelDugal@gmail.com>
2021-03-09 19:58:30 +0600

    Хедер через фэин клиент.

--- src/main/java/jm/stockx/feign/AuthRestHttpServiceClient.java
+++ src/main/java/jm/stockx/feign/AuthRestHttpServiceClient.java
@@ -5,10 +5,12 @@ import jm.stockx.dto.UserTokenDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 
 @FeignClient(name = "Aut", url = "http://localhost:8080")
 public interface AuthRestHttpServiceClient {
 
     @PostMapping(value = "/api/auth/login",  consumes = "application/json")
-    UserTokenDto getToken(@RequestBody UserLoginDto userLoginDto);
+    UserTokenDto getToken(@RequestHeader("Token") String token, @RequestBody UserLoginDto userLoginDto);
 }
--- src/main/java/jm/stockx/http/service/AuthRestHttpService.java
+++ src/main/java/jm/stockx/http/service/AuthRestHttpService.java
@@ -1,10 +1,13 @@
 package jm.stockx.http.service;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import jm.stockx.dto.UserLoginDto;
 import jm.stockx.dto.UserTokenDto;
 import jm.stockx.feign.AuthRestHttpServiceClient;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Data
@@ -14,18 +17,31 @@ public class AuthRestHttpService {
     private final AuthRestHttpServiceClient authRestHttpServiceClient;
     private String role;
 
+    DateSecurityTokenGenerator dateSecurityTokenGenerator = new DateSecurityTokenGenerator();
+
     @Autowired
     public AuthRestHttpService(AuthRestHttpServiceClient authRestHttpServiceClient) {
         this.authRestHttpServiceClient = authRestHttpServiceClient;
     }
+    @Bean
+    public RequestInterceptor requestInterceptor() {
 
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                template.header("Token", dateSecurityTokenGenerator.DateTokenCoder());
+            }
+        };
+    }
     public String getToken(String username, String password) {
 
         UserLoginDto userLoginDto = new UserLoginDto(username, password, true);
+
         try {
+
             UserTokenDto userTokenDto =
-                    authRestHttpServiceClient.getToken(
-                            userLoginDto);
+                    authRestHttpServiceClient
+                            .getToken( dateSecurityTokenGenerator.DateTokenCoder(),userLoginDto);
             this.role = userTokenDto.getRoleName();
             return userTokenDto.getToken();
         } catch (Exception e) {
